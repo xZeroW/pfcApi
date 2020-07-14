5d17e9ec4783bdee9f99118e4ca34ea209a96d20
xZeroW <vic_marts@hotmail.com>
2020-07-14 18:37:09 -0300

    code splitting

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require("cors");
 const helmet = require('helmet');
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
 const cron = require('node-cron');
 const { errors } = require('celebrate');
+const jwtCheck = require('./app/middlewares/auth0');
 
 require('./database');
 const routes = require('./routes');
@@ -21,19 +20,6 @@ var corsOptions = {
     }
   }
 }
-//const appOrigin = process.env.appOrigin;
-
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: process.env.jwksUri
-}),
-audience: process.env.audience,
-issuer: process.env.issuer,
-algorithms: ['RS256']
-});
 
 app.use(cors(corsOptions));
 app.use(jwtCheck);
