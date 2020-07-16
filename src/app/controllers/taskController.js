8b62b6500ac0669984f325647c18e385931a61b2
xZeroW <vic_marts@hotmail.com>
2020-07-16 13:10:02 -0300

    task description not required

--- src/app/controllers/taskController.js
+++ src/app/controllers/taskController.js
@@ -27,7 +27,7 @@ module.exports = {
   
   create: async (req, res) => {
     try {
-      const { title, description, completion_date, status } = req.body;
+      //const { title, description, completion_date, status } = req.body;
       const { projectid } = req.params;
       const { sub } = req.user; // declared but never read
 
@@ -37,13 +37,7 @@ module.exports = {
         return res.status(401).json({ error: 'Unathorized operation' });
       };
 
-     const taskCreated = await Task.create({
-      project_id: projectid,
-      title,
-      description, 
-      completion_date, 
-      status
-     });
+     const taskCreated = await Task.create(req.body);
 
      return res.status(201).json(taskCreated);
 
